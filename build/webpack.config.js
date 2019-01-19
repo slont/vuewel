316382dc42c47a5b0311e974e9c1e544a6ef3995
slont <slont.maytry@gmail.com>
2019-01-19 13:41:32 +0900

    buildの設定追加

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -1,36 +1,59 @@
-require('@babel/register')
-require('babel-loader')
+const path = require('path')
+const fs = require('fs')
 const {VueLoaderPlugin} = require('vue-loader')
+
+const root = './src/components'
+const entry = {
+  'index': './src/index.js',
+  'components/index': [`${root}/index`]
+}
+const files = fs.readdirSync(root)
+files.forEach(component => {
+  if (fs.statSync(path.join(root, component)).isDirectory()) {
+    const entryKey = `components/${component}/index`
+    const entryValue = `${root}/${component}/index`
+    entry[entryKey] = [entryValue]
+  }
+})
 
 module.exports = {
   mode: 'production',
-  entry: './src/index.js',
+  entry,
   output: {
-    path: './dist',
-    filename: '[name].js'
+    path: path.resolve(__dirname, '../dist'),
+    filename: '[name].min.js',
+    library: 'Vuewel'
   },
   module: {
     rules: [
+      {
+        test: /\.ts$/,
+        loader: 'ts-loader',
+        exclude: '/node_modules/',
+        options: {
+          appendTsSuffixTo: [/\.vue$/]
+        }
+      },
       {
         test: /\.vue$/,
         use: 'vue-loader'
       },
       {
         test: /\.(css|scss)$/,
-        loaders: ["style-loader", "css-loader", "sass-loader",
+        loaders: ['style-loader', 'css-loader', 'sass-loader',
           {
-            loader: "sass-resources-loader",
+            loader: 'sass-resources-loader',
             options: {
               resources: ['./src/scss/global.scss'],
               include: path.resolve(__dirname, '../')
             }
           }],
-        include: path.resolve(__dirname, "../")
+        include: path.resolve(__dirname, '../')
       }
     ]
   },
   resolve: {
-    extensions: ['.js', '.vue', '.json', 'scss']
+    extensions: ['.js', '.ts', '.vue', '.json', 'scss']
   },
   plugins: [new VueLoaderPlugin()]
 }
