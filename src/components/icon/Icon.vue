32e13510a01e2993b7dc0386933c64a74ca15b6a
slont <slont.maytry@gmail.com>
2019-01-26 15:04:43 +0900

    Scssを外出し

--- src/components/icon/Icon.vue
+++ src/components/icon/Icon.vue
@@ -22,8 +22,3 @@
     }
   }
 </script>
-
-<style lang="scss">
-  .w-icon {
-  }
-</style>
--- src/components/navtabs/Navtabs.vue
+++ src/components/navtabs/Navtabs.vue
@@ -28,7 +28,7 @@
         const tab = this.tabs[index]
         if (tab.path && this.$router) {
           this.$router.push(tab.path)
-        } else if (tab.url && window) {
+        } else if (tab.url && typeof window !== 'undefined') {
           window.open(tab.url, '_blank')
         }
         this.$emit('click', tab)
@@ -36,44 +36,3 @@
     }
   }
 </script>
-
-<style lang="scss">
-  .w-navtabs {
-    position: relative;
-    display: flex;
-    margin: 0;
-    padding: 0;
-
-    .w-navtab {
-      position: relative;
-      display: inline-flex;
-      padding: .5rem .875rem;
-      align-items: center;
-      cursor: pointer;
-
-      &:after {
-        position: absolute;
-        bottom: 0;
-        left: 0;
-        content: '';
-        width: 100%;
-        height: 2px;
-        background: lighten($main-color, 20%);
-        transform: scale(0, 1);
-        transform-origin: center top;
-        transition: all .3s;
-      }
-      &:hover:after {
-        transform: scale(1, 1);
-      }
-      &.is-active {
-        font-weight: bold;
-
-        &:after {
-          transform: scale(1, 1);
-          background-color: $main-color;
-        }
-      }
-    }
-  }
-</style>
